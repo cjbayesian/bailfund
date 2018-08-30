c99f72c648083f414d51b975909fc5bcd7f24e77
Corey Chivers <corey.chivers@uphs.upenn.edu>
2018-08-29 20:04:52 -0400

    Add option for which type of document to get

--- spawn_batches.sh
+++ spawn_batches.sh
@@ -3,10 +3,11 @@ BIG_FROM=$1
 BIG_TO=$2
 CHUNK_SIZE=$3
 YEAR=$4
+BATCH_SCRIPT=$5
 for i in $(seq $BIG_FROM $BIG_TO); do 
     from=$(($i * $CHUNK_SIZE))
     to=$(($from + $CHUNK_SIZE))
     from=$(($from + 1))
     #echo $from $to 2017
-    ./batches.sh $to $from $YEAR &
+    $BATCH_SCRIPT $to $from $YEAR &
 done
